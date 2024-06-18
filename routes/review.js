ad7f062a73c4071536b966f1e97a9845d90127be
Akshansh Singh Bhadoria <133026950+AkshanshSinghBhadoria@users.noreply.github.com>
2024-06-18 22:57:20 +0530

    Inital Uploading

---
+++ routes/review.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router({mergeParams: true});
+const wrapAsync = require("../utils/wrapAsync.js");
+const Review = require("../models/review.js");
+const Listing = require("../models/listing.js");
+const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
+
+const reviewController = require("../controllers/reviews.js")
+
+//Post Review Route
+router.post("/", validateReview, isLoggedIn, wrapAsync(reviewController.createReview));
+
+//Delete Review Route
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, wrapAsync(reviewController.deleteReview));
+
+module.exports = router;
