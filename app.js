4d346fe71b92e33b8308fd20662ade321b4f8427
Akshansh Singh Bhadoria <133026950+AkshanshSinghBhadoria@users.noreply.github.com>
2024-06-25 20:09:59 +0530

    no such change

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV != "production") {
     require('dotenv').config();
-}
+}                                    // For Updation in code: (1) git add . (2) git commit -m "message" (3) git push origin main
 
 const express = require("express");
 const app = express();
@@ -88,16 +88,6 @@ app.use((req, res, next) => {
     res.locals.currUser = req.user;
     next();
 });
-
-// app.get("/demouser", async(req, res) => {
-//     let fakeUser = new User({
-//         email: "student@gmail.com",
-//         username: "Akshansh",
-//     });
-
-//     let registeredUser = await User.register(fakeUser, "helloWorld");
-//     res.send(registeredUser);
-// });
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
