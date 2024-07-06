7793eed7dbe42f925ca27bf7b8f8e7e8451aec2c
Akshansh Singh Bhadoria <133026950+AkshanshSinghBhadoria@users.noreply.github.com>
2024-07-06 12:28:47 +0530

    removed push steps

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV != "production") {
     require('dotenv').config();
-}                                    // For Updation in code: (1) git add . (2) git commit -m "message" (3) git push origin main
+}                                    
 
 const express = require("express");
 const app = express();
